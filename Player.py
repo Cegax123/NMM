ada3b268595832c2435d2d66056dad12889e5604
bardend <ricardo.ulloa.v@uni.pe>
2022-05-11 22:52:28 -0500

    Json Conf

--- Player.py
+++ Player.py
@@ -2,4 +2,4 @@ class Player:
     def __init__(self, piece_color, order, remain_pieces):
         self.piece_color = piece_color
         self.remain_pieces = remain_pieces
-        self.order = order # 0 : First player, 1 : Second player
+        self.order = order
